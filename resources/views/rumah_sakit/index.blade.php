fa624017e5a337d9983b29094869732813b15bfb
ahyaghifari <ahyaghifari288@gmail.com>
2026-06-03 15:57:46 +0800

    update layanan unggulan section

--- resources/views/rumah_sakit/index.blade.php
+++ resources/views/rumah_sakit/index.blade.php
@@ -336,7 +336,7 @@
             </div>
 
             {{-- Grid kartu --}}
-            <div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-5">
+            <div class="grid grid-cols-2 lg:grid-cols-3 gap-5">
                 @foreach($layanan_unggulan as $lu)
                 <a wire:navigate href="{{ rumahsakit_route('rumahsakit.unggulan') }}"
                    class="group flex flex-col items-center bg-white/10 hover:bg-white/20
