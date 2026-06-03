3959e70ae3f37e7c6858729f144e388e682f98d5
ahyaghifari <ahyaghifari288@gmail.com>
2026-06-03 13:27:14 +0800

    update hubungi kami page grid

--- resources/views/rumah_sakit/pages/hubungi-kami.blade.php
+++ resources/views/rumah_sakit/pages/hubungi-kami.blade.php
@@ -14,7 +14,7 @@
                 </span>
             </div>
 
-            <div class="grid grid-cols-2 md:grid-cols-3 gap-5">
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
                 @foreach($operasional as $item)
                 @php
                     $icon = 'call';
