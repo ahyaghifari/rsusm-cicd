139116e142438dcd09eb02aebadd338c35f7f557
ahyaghifari <ahyaghifari288@gmail.com>
2026-08-06 16:29:15 +0800

    delete default filter

--- app/Filament/Resources/JadwalHarianResource/Pages/JadwalHarianPage.php
+++ app/Filament/Resources/JadwalHarianResource/Pages/JadwalHarianPage.php
@@ -188,8 +188,8 @@ class JadwalHarianPage extends Page
             ->whereHas('poliklinik', function ($q) use ($rsId) {
                 $q->where('rumah_sakit_id', $rsId)->where('jadwal_tidak_tetap', false);
             })
-            ->when($this->executiveClinicFilter === 'reguler',   fn ($q) => $q->where('is_executive', 0))
-            ->when($this->executiveClinicFilter === 'eksekutif', fn ($q) => $q->where('is_executive', 1))
+            // ->when($this->executiveClinicFilter === 'reguler',   fn ($q) => $q->where('is_executive', 0))
+            // ->when($this->executiveClinicFilter === 'eksekutif', fn ($q) => $q->where('is_executive', 1))
             ->get();
 
         $newRows = [];
@@ -320,7 +320,7 @@ class JadwalHarianPage extends Page
         $this->rowsCache             = [];
         $this->selectedUnitLayananId = null;
         $this->rows                  = [];
-        $this->executiveClinicFilter = 'all';
+        // $this->executiveClinicFilter = 'reguler';
 
         if ($this->selectedRumahSakitId) {
             $this->loadRows();
