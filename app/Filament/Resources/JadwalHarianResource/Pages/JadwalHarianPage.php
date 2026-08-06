9d5e98c8d78009637c7a0b19786a66f602fc0b55
ahyaghifari <ahyaghifari288@gmail.com>
2026-08-06 16:24:57 +0800

    delete default filter

--- app/Filament/Resources/JadwalHarianResource/Pages/JadwalHarianPage.php
+++ app/Filament/Resources/JadwalHarianResource/Pages/JadwalHarianPage.php
@@ -318,7 +318,7 @@ class JadwalHarianPage extends Page
         $this->rowsCache             = [];
         $this->selectedUnitLayananId = null;
         $this->rows                  = [];
-        $this->executiveClinicFilter = 'reguler';
+        // $this->executiveClinicFilter = 'reguler';
 
         if ($this->selectedRumahSakitId) {
             $this->loadRows();
